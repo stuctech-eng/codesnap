6cec20f53ae72bf1d31589f917083102e27324f2
stuctech-eng <266315036+stuctech-eng@users.noreply.github.com>
2026-04-26 12:39:14 +0200

    icoon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="nl">
+      <head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="CodeSnap" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body>{children}</body>
     </html>
   );
